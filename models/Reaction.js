a4d1491c645c051ef79cf8a799940617938b97fc
Nathan Difiori <ndifiori@gmail.com>
2023-01-18 20:41:32 -0800

    models created

---
+++ models/Reaction.js
@@ -0,0 +1,48 @@
+const { Schema, Types} = require('mongoose');
+const dateFormat = require('../utils/dateFormat')
+
+// create a new schema object called reaction schema
+  // reactionId field 
+    // Types.objectID constructor which creates an unique id
+  // reactionBody field
+    // type = string
+  // username field
+    // type = string
+  // createdAt field
+    // date with getter function
+
+  // options added to object
+    // toJson = documents from collection converted to JSON when returned from db
+    // id field = will not be included when returned
+ 
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String, 
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: timestamp => dateFormat(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+module.exports = reactionSchema;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+
+require('dotenv').config()
+
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const PORT = process.env.port || 3001;
-const app = express()
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
